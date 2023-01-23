5a914bf993664ce7bad1ba3f06226a5779877895
Hagen Strahl <hagen.strahl@gmail.com>
2023-01-23 17:32:07 +0100

    add cta button to internal link

--- apps/website/components/block-content/internalLink.tsx
+++ apps/website/components/block-content/internalLink.tsx
@@ -1,17 +1,27 @@
 import NextLink from "next/link";
+import Button from "../shared/button";
 
 interface InternalLinkProps {
   mark: {
     docType: string;
+    isCTA: boolean;
     slug: string;
   };
   children: string;
 }
 
+const buildChildren = (
+  isCTA: boolean,
+  children: JSX.Element | string
+): JSX.Element | string => {
+  if (isCTA) return <Button size="small">{children}</Button>;
+  else return children;
+};
+
 const InternalLink = ({ mark, children }: InternalLinkProps): JSX.Element => {
   return (
     <NextLink href={`/${mark.docType}/${mark.slug}`}>
-      <a className="font-bold">{children}</a>
+      <a className="font-bold">{buildChildren(mark.isCTA, children)}</a>
     </NextLink>
   );
 };
