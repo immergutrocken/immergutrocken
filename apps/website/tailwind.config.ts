8436770a0f4ccea5298522bf22e3c67d2d95aa29
Hagen Strahl <hagen.strahl@drklein-wowi.de>
2024-12-02 13:23:04 +0100

    change tertiary color for new motto

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
       colors: {
         primary: "#000",
         secondary: "#FFF",
-        tertiary: "#98CE58",
+        tertiary: "#FF4800",
       },
       invert: {
         partner: "0",
